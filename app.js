faa9414730bcd72e0eeb0cb49d20428a826ef174
Angelo Almeida <angelo94.aa@gmail.com>
2022-10-03 14:01:53 -0500

    actualizandl javascript

--- app.js
+++ app.js
@@ -14,9 +14,23 @@ function clickBtnCalcular() {
 	const pintar = document.getElementById('txtPintar')
 	pintar.innerHTML = `<span>Su peso es: ${imc}</span>` + Math.round(imc)
 	// pintar = document.write('Su IMC es: ' + Math.round(imc))
+	const alert = document.getElementById('alert')
+	if (imc < 18.5) {
+		alert.innerHTML = `<p>Tiene bajo peso</p>`
+	} else if (imc >= 18.5 || imc <= 24.9) {
+		alert.innerHTML = `<p>Tiene un peso adecuado</p>`
+	} else if (imc > 25.0 || imc <= 29.9) {
+		alert.innerHTML = `<p>Tiene sobrepeso</p>`
+	} else if (imc == 30.0 || imc <= 34.9) {
+		alert.innerHTML = `<p>Tiene obesidad grado 1</p>`
+	} else if (imc === 35.0 || imc <= 39.9) {
+		alert.innerHTML = `<p>Tiene obesidad grado 2</p>`
+	} else {
+		alert.innerHTML = `<p>Tiene obesidad grado 3</p>`
+	}
 }
 
-function clickBtnRemove() {
-	let btnReset = document.getElementById('btnReset')
-	btnReset.addEventListener('click', clickBtnRemove)
-}
+/* function iniciar() {
+	document.getElementById('btnReset').addEventListener('click', clickBtnRemove)
+	// btnReset.addEventListener('click', clickBtnRemove)
+} */
